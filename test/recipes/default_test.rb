a2883c1431319441aa1018d82b6092e493e0dfb1
okram999 <nirish.okram@gmail.com>
2016-12-19 14:31:51 -0600

    Adds the inspec test

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,14 +5,42 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+# unless os.windows?
+#   describe user('root') do
+#     it { should exist }
+#     skip 'This is an example test, replace with your own test.'
+#   end
+# end
+#
+# describe port(80) do
+#   it { should_not be_listening }
+#   skip 'This is an example test, replace with your own test.'
+# end
+describe user('nexus') do
+  it { should exist }
+  its('group') { should eq 'nexus' }
+  its('home') { should eq '/nexus' }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe file('/usr/local/nexus') do
+ it { should exist }
+end
+
+describe file('/usr/local/nexus/current') do
+  its('type') { should eq 'link' }
+  it { should be_symlink }
+  it { should_not be_file }
+end
+
+describe file('/opt/mount1/nexus/sonatype-work/nexus') do
+ it { should exist }
+end
+
+#%r{} is equivalent to the /.../ notation
+describe file('/usr/local/nexus/current/nexus-2.12.0-01/conf/nexus.properties') do
+  its('content') { should match(%r{^nexus-work=/opt/mount1/nexus/sonatype-work/nexus}) }
+end
+
+describe file('/usr/local/nexus/shared/pids/nexus.pid') do
+ it { should exist }
 end
